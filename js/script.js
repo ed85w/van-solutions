5302832c76a7d03e69e1bd808ee40d08b7a42769
ed85w <ed.walker@hotmail.co.uk>
2019-02-05 22:57:49 +0000

    navbar working, needs tidy and style

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('#overlay').addClass('overlay-fade');
+
+    // sidebar functions
+    $('.sidebarBtn').click(function(){
+        $('.sidebar').toggleClass('active')
+        $('.sidebarBtn').toggleClass('toggle')
+    })
+      
 })
