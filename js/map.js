6800ab30045c528bb9a62a8452d4a2b2ab279172
ed85w <ed.walker@hotmail.co.uk>
2019-03-03 21:49:35 +0000

    map and form added

---
+++ js/map.js
@@ -0,0 +1,39 @@
+// Initialize and add the map
+function initMap() {
+
+  // The location of tinyStompers
+  var vanSolutions = {lat: 54.024630, lng: -1.157781};
+
+  // Map options
+  var options = {
+    zoom: 14,
+    center: vanSolutions,
+    // styles: [{
+    //   "stylers": [{
+    //       "saturation": -100
+    //   }, {
+    //       "gamma": 1
+    //   }]
+    // }]
+  }
+
+  // The map, centered at vanSolutions
+  var map = new google.maps.Map(
+      document.getElementById('map'), options);
+
+  // // The icon
+  // var icon = {
+  //   url: "./img/tiny-stompers-logo-pin.png", // url
+  //   scaledSize: new google.maps.Size(40, 40), // scaled size
+// };
+
+  // The marker, positioned at tinyStompers
+  var marker = new google.maps.Marker({
+    position: vanSolutions, 
+    map: map,
+    // icon: icon,
+  });
+
+
+   
+}
